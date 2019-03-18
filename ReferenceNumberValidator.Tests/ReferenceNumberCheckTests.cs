3e0b1189750672afd3389f0fc97cac6bdc2a5483
Panu Oksala <mino@hehkulamppu.net>
2019-03-18 15:07:20 +0200

    Added missing unit tests

---
+++ ReferenceNumberValidator.Tests/ReferenceNumberCheckTests.cs
@@ -0,0 +1,46 @@
+using Xunit;
+
+namespace ReferenceNumberValidator.Tests
+{
+    public class ReferenceNumberCheckTests
+    {
+        [Fact]
+        public void Valid_Reference_Number()
+        {
+            var validReferenceNumber = "12304561";
+            Assert.True(FinnishReferenceNumberValidator.ReferenceNumberValidator.IsValidFinnishReference(validReferenceNumber).IsValid);
+        }
+
+        [Fact]
+        public void Invalid_Reference_CheckSum()
+        {
+            var invalidCheckSum = "12304562";
+            Assert.False(FinnishReferenceNumberValidator.ReferenceNumberValidator.IsValidFinnishReference(invalidCheckSum).IsValid);
+        }
+
+        [Fact]
+        public void Too_Long_Reference_Number()
+        {
+            var longReferenceNumber = "123456789012345678901";
+            Assert.False(FinnishReferenceNumberValidator.ReferenceNumberValidator.IsValidFinnishReference(longReferenceNumber).IsValid);
+        }
+
+        [Fact]
+        public void Too_Short_Reference_Number()
+        {
+            var shortReferenceNumber = "123";
+            Assert.False(FinnishReferenceNumberValidator.ReferenceNumberValidator.IsValidFinnishReference(shortReferenceNumber).IsValid);
+        }
+
+        [Fact]
+        public void Non_Numeric_Reference_Number()
+        {
+            var nonDigitReferenceNumber = "A12304561";
+            
+            var result = FinnishReferenceNumberValidator.ReferenceNumberValidator.IsValidFinnishReference(nonDigitReferenceNumber);
+
+            Assert.False(result.IsValid);
+            Assert.Contains("only numeric values", result.ValidationMessage);
+        }
+    }
+}
